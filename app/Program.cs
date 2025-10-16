d8cf46d47ce36d0c884b5b48d770053b649a99b7
vot-blin <xmedli@bk.ru>
2025-10-16 15:39:45 +0300

    Update project with MGINX

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace project;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-app.MapGet("/", () => "Hello from C#!");
+        app.MapGet("/", () => "Hello from C#!");
+        app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }));
 
-app.Run("http://0.0.0.0:8080");
+        app.Run("http://0.0.0.0:8080");
+    }
+}
--- obj/Debug/net9.0/project.AssemblyInfo.cs
+++ obj/Debug/net9.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cbccc8525d2b878cf837ceeb24391b97058d405")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
